d07a2e5659564f2b724ac25a53dba75943de72d6
Sravanthi <sravanthidasari@hotmail.com>
2020-03-14 10:07:37 -0700

    Added bootstrap stylesheet

--- corona_app/src/App.js
+++ corona_app/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <div className="container">
+            <h1> Corona Stats </h1>
+            <h3>Name</h3>
+            <h3>Age</h3>
+            <h3>Gender</h3>
+            <h3>Location</h3>
+            <h3>
+                Symptons
+                <h4>New</h4>
+                <h4>Fever</h4>
+                <h4>SOB</h4>
+                <h4>Cough</h4>
+            </h3>
+            <h3>Tested</h3>
+        </div>
+    )
+  }
 }
 
 export default App;
