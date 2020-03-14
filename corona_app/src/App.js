0c5123597851b599bf1c832598494b79125ab194
Scott Draper <draper.tscott@gmail.com>
2020-03-14 12:57:13 -0600

    Results component linked and functional, now displaying a div

--- corona_app/src/App.js
+++ corona_app/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Results from './components/Results.js'
 
 class App extends React.Component {
   render() {
-    return <div>Hey!</div>
+    return (
+      <div>
+        Hey!
+
+        <Results/>
+        
+      </div>
+    )
   }
 }
 
